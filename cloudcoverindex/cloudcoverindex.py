9d941ff37d2bd967c41a5c8a7624d077af44de28
Daniel Linares Gil <daniel.linares@ciencias.unam.mx>
2020-11-20 23:46:44 -0600

    Base Project Structure

---
+++ cloudcoverindex/cloudcoverindex.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+
+def mask_filter(image, mask):
+    pass #TODO Implement this method
+
+def red_blue_filter(image):
+    pass #TODO Implement this method
+
+def convolution_filter(image):
+    pass #TODO Implement this method
+
+class CloudCoverApp:
+    image
+
+    def __init__(path):
+        pass #TODO Implement this method
+
+    def get_cloud_cover_index():
+        pass #TODO Implement this method
+
+    def save(path):
+        pass #TODO Implement this method
